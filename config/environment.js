9abac30a4827a9ad1b1a5935002fa58c47981384
Ilya Radchenko <ilya@burstcreations.com>
2017-06-05 23:10:14 -0400

    add root url config

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,13 @@
 /* eslint-env node */
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function(environment/*, appConfig */) {
+  var ENV = {};
+
+  if (environment === 'production') {
+    ENV.rootURL = '/milligram-app';
+    ENV.locationType = 'hash';
+  }
+
+  return ENV;
 };
