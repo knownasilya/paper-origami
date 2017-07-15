f4d14c207cf8855b6a84964f30ecb8ab0f6159db
Ilya Radchenko <ilya@burstcreations.com>
2017-07-15 08:43:50 -0400

    fix: add head content and make styles manual import

--- index.js
+++ index.js
@@ -7,5 +7,15 @@ module.exports = {
   included: function() {
     this._super.included.apply(this, arguments);
     this.import('vendor/paper-origami/style.css');
+  },
+
+  contentFor(type) {
+    if (type === 'head') {
+      return [
+        '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">',
+        '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">',
+        '<link href="css/materialdesignicons.min.css" media="all" rel="stylesheet" type="text/css" />'
+      ].join('\n');
+    }
   }
 };
