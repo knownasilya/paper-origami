ad65254659fc1b2c07614193d63d90e01acee45d
Ilya Radchenko <ilya@burstcreations.com>
2017-08-03 21:53:13 -0400

    feat(page): Added a footer with page navigation e.g. next/prev

--- addon/components/origami-page/component.js
+++ addon/components/origami-page/component.js
@@ -1,10 +1,45 @@
 import Ember from 'ember';
 import layout from './template';
 
-const { inject } = Ember;
+const { inject, computed } = Ember;
 
 export default Ember.Component.extend({
   origami: inject.service(),
+  router: inject.service(),
   layout,
-  classNames: ['origami-page']
+  classNames: ['origami-page'],
+
+  nextNavigation: computed('router.currentRouteName', 'origami.navigation', {
+    get() {
+      let currentRouteName = this.get('router.currentRouteName');
+      let navigation = this.get('origami.navigation');
+      let current = navigation.findBy('route', currentRouteName);
+      let currentIndex = navigation.indexOf(current);
+
+      return navigation.length > currentIndex ? navigation.objectAt(currentIndex + 1) : undefined;
+    }
+  }),
+
+  previousNavigation: computed('router.currentRouteName', 'origami.navigation', {
+    get() {
+      let currentRouteName = this.get('router.currentRouteName');
+      let navigation = this.get('origami.navigation');
+      let current = navigation.findBy('route', currentRouteName);
+      let currentIndex = navigation.indexOf(current);
+
+      return currentIndex !== 0 ? navigation.objectAt(currentIndex - 1) : undefined;
+    }
+  }),
+
+  actions: {
+    transitionTo(...args) {
+      let last = args[args.length - 1];
+      
+      if (last instanceof Ember.$.Event) {
+        args = args.slice(0, -1);
+      }
+      
+      this.get('router').transitionTo(...args);
+    }
+  }
 });
